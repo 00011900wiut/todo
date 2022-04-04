2694a5348feb37b6723e02175c407a802b8fda4c
00011900wiut <94618174+00011900wiut@users.noreply.github.com>
2022-04-04 18:03:45 +0500

    connected to database

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,6 +1,32 @@
+const mySql = require('mysql')
+
+// Connection port
+const pool = mySql.createPool({
+    connectionLimit : 100,
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database : process.env.DB_NAME
+});
 
 // View users
-
 exports.view = (req, res) => {
-    res.render('home');
-}
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err; // problem occured!
+        console.log('Successfully connected as ID ' + connection.threadId);
+
+        // User connection
+        connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
+            // Release it, when done with the connection
+            connection.release();
+
+            if (!err) {
+                res.render('home', { rows })
+            } else {
+                console.log(err); // problem occured!
+            }
+
+        });
+    });
+};
